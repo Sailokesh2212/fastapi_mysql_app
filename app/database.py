b71913429c46a35beacee9c57ea224a4b9677d51
Sailokesh2212 <lohithsaimv@gmail.com>
2026-01-22 14:14:40 +0000

    Use DATABASE_URL env var correctly

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(postgresql://fastapi_db_eq51_user:ExMoSDWULrWbThJ03g9uE1agtIKsq31S@dpg-d5p2kutactks7395fm90-a/fastapi_db_eq51)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(
